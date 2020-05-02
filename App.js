a104b4c4a2e7d560ae3159d77d318bcae78c1595
llucaslleall <41243990+llucaslleall@users.noreply.github.com>
2020-05-02 19:53:21 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Navbar, Login} from './componentes';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Login />
+    </>
+  );
+}
+
+export default App;
