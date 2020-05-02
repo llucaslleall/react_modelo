bb85b28adea4adc695b902dd35e9d71916236301
llucaslleall <41243990+llucaslleall@users.noreply.github.com>
2020-05-02 19:56:41 -0300

    Add files via upload

---
+++ parametros/navbar.js
@@ -0,0 +1,15 @@
+export default {
+    tituloPrincipal: 'Título Principal',
+    linkPrincipal: './',
+
+    //Caso não queira usar alguma das opções basta comentá-la
+    item1: 'Página 1',
+    link1: 'pagina1',
+    //item2: '',
+    //link2: '',
+    //item3: '',
+    //link3: '',
+ 
+    login: 'Login',
+    linkLogin: 'paginaLogin',
+}
