6208cb459d8a887081b2f28f11ee18ca96b5b21d
llucaslleall <41243990+llucaslleall@users.noreply.github.com>
2020-05-03 13:54:49 -0300

    Add files via upload

---
+++ parametros/cadastro.js
@@ -0,0 +1,13 @@
+export default {
+    titulo: 'Cadastro',
+    placeholder1: 'E-mail',
+    placeholder2: 'Senha',
+    msgLoading: 'Loading...',
+    botao: 'Cadastrar',    
+    
+    //Textos no caso de sucesso e erro
+    strongSucesso: 'Opa, tudo certo!',
+    textoSucesso: 'Direcionando para a página...',
+    strongErro: 'Ops, algo deu errado :(',
+    textoErro: 'Por favor, tente novamente o login',
+}
--- parametros/index.js
+++ parametros/index.js
@@ -1,4 +1,5 @@
 import ParNavbar from './navbar';
 import ParLogin from './login';
+import ParCadastro from './cadastro';
 
-export { ParNavbar, ParLogin };
+export { ParNavbar, ParLogin, ParCadastro };
