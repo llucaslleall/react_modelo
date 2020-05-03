208ff005626ba573d05b9d0d0a508b3166870756
llucaslleall <41243990+llucaslleall@users.noreply.github.com>
2020-05-03 13:56:00 -0300

    Create firebase.js

---
+++ config/firebase.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase';
+
+//Colar aqui o que tiver dentro da tag <script> - caso já tenha alguma coisa, apagar tudo menos a última linha
+//Após colar, trocar var por const
+
+// INSERIR export default ANTES DA ÚLTIMA LINHA ACIMA
